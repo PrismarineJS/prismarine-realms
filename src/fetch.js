66456b54d01ec1867c933102d5d0ff3025077a9a
LucienHH <Lucien.holloway@aprox.co.uk>
2022-03-10 00:48:27 +0000

    Change User-Agent header

--- src/fetch.js
+++ src/fetch.js
@@ -1,17 +1,12 @@
 const { checkStatus } = require('./util')
 const fetch = require('node-fetch')
 
-const baseHeaders = (additionalHeaders = {}) => ({
-  'User-Agent': '@prismarineJS/prismarine-realms',
-  'Client-Version': '0.0.0',
-  ...additionalHeaders
-})
-
 module.exports = (url, options = {}) => auth => {
   return fetch(url, {
     ...options,
     headers: {
-      ...baseHeaders(options.headers),
+      'Client-Version': '0.0.0',
+      ...options.headers,
       ...auth
     }
   }).then(checkStatus)
--- src/util.js
+++ src/util.js
@@ -13,11 +13,11 @@ async function checkStatus (res) {
 
 function formatJavaAuth (res) {
   if (!res.profile?.id || !res.profile?.name) throw new Error('Failed to obtain profile data, does the authenticating account own minecraft?')
-  return { cookie: `sid=token:${res.token}:${res.profile.id}; user=${res.profile.name}; version=0.0.0` }
+  return { cookie: `sid=token:${res.token}:${res.profile.id}; user=${res.profile.name}; version=0.0.0`, 'User-Agent': 'MinecraftLauncher/2.2.10675' }
 }
 
 function formatBedrockAuth (res) {
-  return { authorization: `XBL3.0 x=${res.userHash};${res.XSTSToken}` }
+  return { authorization: `XBL3.0 x=${res.userHash};${res.XSTSToken}`, 'User-Agent': 'MCPE/UWP' }
 }
 
 module.exports = {

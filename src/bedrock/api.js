61b0797197b2ba4d4b9c12886603f51196ff17b2
LucienHH <lucienharryholloway@hotmail.co.uk>
2022-05-10 12:29:49 +0100

    Add invite endpoints

--- src/bedrock/api.js
+++ src/bedrock/api.js
@@ -18,6 +18,67 @@ module.exports = class BedrockRealmAPI extends RealmAPI {
     return new Realm(this, data)
   }
 
+  async getRealmInvite (realmId) {
+    const data = await this.rest.get(`/links/v1?worldId=${realmId}`)
+    return {
+      inviteCode: data[0].linkId,
+      ownerXUID: data[0].profileUuid,
+      type: data[0].type,
+      createdOn: data[0].ts,
+      inviteLink: data[0].url,
+      deepLinkUrl: data[0].deepLinkUrl
+    }
+  }
+
+  async refreshRealmInvite (realmId) {
+    const data = await this.rest.post('/links/v1', {
+      body: {
+        type: 'INFINITE',
+        worldId: realmId
+      }
+    })
+    return {
+      inviteCode: data.linkId,
+      ownerXUID: data.profileUuid,
+      type: data.type,
+      createdOn: data.ts,
+      inviteLink: data.url,
+      deepLinkUrl: data.deepLinkUrl
+    }
+  }
+
+  async getPendingInviteCount () {
+    return await this.rest.get('/invites/count/pending')
+  }
+
+  async getPendingInvites () {
+    const data = await this.rest.get('/invites/pending')
+    return data.invites.map(invite => {
+      return {
+        invitationId: invite.invitationId,
+        worldName: invite.worldName,
+        worldDescription: invite.worldDescription,
+        worldOwnerName: invite.worldOwnerName,
+        worldOwnerXUID: invite.worldOwnerUuid,
+        createdOn: invite.date
+      }
+    })
+  }
+
+  async acceptRealmInvitation (invitationId) {
+    await this.rest.put(`/invites/accept/${invitationId}`)
+  }
+
+  async rejectRealmInvitation (invitationId) {
+    await this.rest.put(`/invites/reject/${invitationId}`)
+  }
+
+  async acceptRealmInviteFromCode (inviteCode) {
+    if (!inviteCode) throw new Error('Need to provide a realm invite code/link')
+    const clean = inviteCode.replace(/https:\/\/realms.gg\//g, '')
+    await this.rest.post(`/invites/v1/link/accept/${clean}`)
+  }
+
   async invitePlayer (realmId, uuid) {
     const data = await this.rest.put(`/invites/${realmId}/invite/update`, {
       body: {

7ea206fb79e1bc29ca157291bfd1237417ac6a40
NoVa Gh0ul <146355400+NoVa-Gh0ul@users.noreply.github.com>
2025-12-15 21:04:58 -0500

    fixes p2

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ module.exports = {
   bedrock: {
     host: 'https://pocket.realms.minecraft.net',
     userAgent: 'MCPE/UWP',
-    relyingParty: 'https://pocket.realms.minecraft.net'
+    relyingParty: 'https://pocket.realms.minecraft.net/'
   },
   java: {
     host: 'https://pc.realms.minecraft.net',
